9feff1b4366a2be10d3ffbd1af7f4906d1f9c5e0
Albert-Jan Nijburg <albertjan@curit.com>
2015-08-31 16:16:53 +0200

    install acuminous

--- blueprints/ember-cli-yadda/index.js
+++ blueprints/ember-cli-yadda/index.js
@@ -1,5 +1,10 @@
 module.exports = {
-  description: ''
+  description: '',
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   // locals: function(options) {
   //   // Return custom template variables here.
@@ -8,7 +13,11 @@ module.exports = {
   //   };
   // }
 
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
+  afterInstall: function() {
+      var addonContext = this;
+
+      return this.addBowerPackagesToProject([
+        { name: 'yadda',           source: 'acuminous/yadda',                     target: '~0.15.1' },
+      ]);
+    }
 };
