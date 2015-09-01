16077210da04d91bc7420e0e07a4ac41b83b4b74
Albert-Jan Nijburg <albertjan@curit.com>
2015-09-01 12:57:10 +0200

    make steps async too

--- tests/acceptance/steps/make-feature-steps.js
+++ tests/acceptance/steps/make-feature-steps.js
@@ -2,7 +2,8 @@ import steps from './steps';
 
 export default function(assert) {
   return steps(assert)
-    .then('I should find a file', function() {
+    .then('I should find a file', function(next) {
       assert.ok(true, this.step);
+      next();
     });
 }
