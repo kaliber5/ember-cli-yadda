a81c35792e71d12bb85b21c7aa3482796eadd63b
Remko Boschker <remko@informatietuin.nl>
2015-09-24 17:28:35 +0200

    fix test use context in a scenario

--- tests/acceptance/steps/use-context-in-a-scenario-steps.js
+++ tests/acceptance/steps/use-context-in-a-scenario-steps.js
@@ -1,4 +1,4 @@
-import steps from '/steps';
+import steps from './steps';
 
 // step definitions that are shared between features should be moved to the
 // tests/acceptance/steps/steps.js file
@@ -7,10 +7,11 @@ export default function(assert) {
   return steps(assert)
     .given('I add something to the context', function(next) {
       this.ctx.something = 'there';
+      assert.ok(true, this.step);
       next();
     })
     .then('it should be there in the next step', function(next) {
-      assert.equal(this.ctx.something, 'there');
+      assert.equal(this.ctx.something, 'there', this.step);
       next();
     });
 }
