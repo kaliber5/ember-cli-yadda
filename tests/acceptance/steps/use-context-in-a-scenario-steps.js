54984f5a3fd80b538785bb358fba6d6e6e396a46
Remko Boschker <remko@informatietuin.nl>
2015-09-24 17:18:51 +0200

    adds context object to a scenario's steps

---
+++ tests/acceptance/steps/use-context-in-a-scenario-steps.js
@@ -0,0 +1,16 @@
+import steps from '/steps';
+
+// step definitions that are shared between features should be moved to the
+// tests/acceptance/steps/steps.js file
+
+export default function(assert) {
+  return steps(assert)
+    .given('I add something to the context', function(next) {
+      this.ctx.something = 'there';
+      next();
+    })
+    .then('it should be there in the next step', function(next) {
+      assert.equal(this.ctx.something, 'there');
+      next();
+    });
+}
---
+++ tests/acceptance/use-context-in-a-scenario.feature
@@ -0,0 +1,6 @@
+Feature: use context in a scenario
+
+  Scenario: I use context
+
+    Given I add something to the context
+    Then it should be there in the next step
