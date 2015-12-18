548249f3378a71bd774f801c3c20e260370103c6
Michael Schinis <mschinis@gmail.com>
2015-12-18 13:14:19 +0200

    fix(unit): Fixes unit test steps

--- blueprints/qunit/ember-cli-yadda/files/tests/unit/steps/steps.js
+++ blueprints/qunit/ember-cli-yadda/files/tests/unit/steps/steps.js
@@ -2,12 +2,11 @@ import yadda from '../../helpers/yadda';
 
 export default function(assert) {
   return yadda.localisation.English.library()
-    .given('I type "Ember g feature make-feature"', (next) => {
-      visit('/');
+    .given('I type "Ember g feature make-feature"', function(next){
       assert.ok(true, this.step);
-      andThen(() => next());
+      next();
     })
-    .when('I look in the folder', (next) => {
+    .when('I look in the folder', function(next){
       assert.ok(true, this.step);
       next();
     });
--- blueprints/qunit/feature-unit/files/tests/unit/steps/__folder__/__name__-steps.js
+++ blueprints/qunit/feature-unit/files/tests/unit/steps/__folder__/__name__-steps.js
@@ -1,11 +1,11 @@
 import steps from '<%= foldersUp %>steps';
 
 // step definitions that are shared between features should be moved to the
-// tests/acceptance/steps/steps.js file
+// tests/unit/steps/steps.js file
 
 export default function(assert) {
   return steps(assert)
-    .then('I should find a file', (next) => {
+    .then('I should find a file', function(next){
       assert.ok(true, this.step);
       next();
     });
