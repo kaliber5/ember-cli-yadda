edc75485091484e1c0c5f23c4235b2eaed2bf3f0
Ben Walder <ben.walder.ctyt@statefarm.com>
2016-11-15 21:22:48 -0600

    use yadda from npm instead of bower

--- blueprints/main-index.js
+++ blueprints/main-index.js
@@ -2,21 +2,11 @@ module.exports = {
   name: 'ember-cli-yadda',
   description: 'ember-cli-yadda',
   normalizeEntityName: function() {
-    // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter
-    // to us
   },
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
   afterInstall: function() {
-    return this.addBowerPackagesToProject([
-      { name: 'yadda',           source: 'acuminous/yadda',                     target: '*' },
-    ]);
+    return this.addPackageToProject('yadda')
+    .then(()=> {
+      return this.addAddonToProject('ember-browserify');
+    });
   }
 };
