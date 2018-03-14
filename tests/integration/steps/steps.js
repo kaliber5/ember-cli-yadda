d4c67f3a3264e93bf99cc9bc8a3bb81449fbae62
Ben Walder <ben.walder.ctyt@statefarm.com>
2018-03-14 13:21:11 -0500

    update dummy app generated files

--- tests/integration/steps/steps.js
+++ tests/integration/steps/steps.js
@@ -1,13 +1,11 @@
 import yadda from '../../helpers/yadda';
-import { visit } from '@ember/test-helpers';
 
 export default function(assert) {
   return yadda.localisation.default.library()
-    .given('I type "Ember g feature make-feature"', async function() {
-      await visit('/');
+    .given('I type "Ember g feature make-feature"', function(){
       assert.ok(true, this.step);
     })
-    .when('I look in the folder', function() {
+    .when('I look in the folder', function(){
       assert.ok(true, this.step);
     });
 }
